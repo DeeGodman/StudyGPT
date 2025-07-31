5deb1b2c0c770fc90ba4e7f48f8cc62c3b6e0d45
Dela-Micke <jehovahisking123@gmail.com>
2025-07-31 01:58:17 +0000

    allow origin with vercel with comma

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://study-gpt-n3ub.vercel.app", "http://localhost:3000"] # In production, specify your frontend URL
+    allow_origins=["https://study-gpt-n3ub.vercel.app"] # In production, specify your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
